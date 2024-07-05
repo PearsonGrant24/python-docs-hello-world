9d3400c6aa67123d0c337347d5612bb764ebb07f
PearsonGrant24 <100286838+PearsonGrant24@users.noreply.github.com>
2024-07-05 15:20:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World! Pear. We want an example of speach sythnesys in Englich and SHona"
+    return "Hello, World! Pear. We want an example of speach synthesis in Englich and SHona"
     
